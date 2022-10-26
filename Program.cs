c510abd62a7107197d36aaa127a95c27da5cc59d
IgorDelic <igordelic17@gmail.com>
2022-10-26 15:41:11 +0200

    Done

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppLabos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Igor Delić");
+        }
+    }
+}
